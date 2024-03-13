ec496db4b83a3d9b83d6148d8b27d605357ba8c7
Kevin Lewis <kvn@lws.io>
2024-03-13 10:06:42 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,90 @@
+export default {
+	id: 'directus-labs-ai-image-generation',
+	name: 'AI Image Generation',
+	icon: 'draw',
+	description: 'Use AI to generate images, powered by DALL•E 3',
+	overview: ({ prompt, quality, size }) => [
+		{
+			label: 'Prompt',
+			text: prompt,
+		},
+		{
+			label: 'Quality',
+			text: quality,
+		},
+		{
+			label: 'Size',
+			text: size,
+		},
+	],
+	options: [
+		{
+			field: 'apiKey',
+			name: 'OpenAI API Key',
+			type: 'string',
+			required: true,
+			meta: {
+				width: 'full',
+				interface: 'input',
+			},
+		},
+		{
+			field: 'prompt',
+			name: 'Prompt',
+			type: 'string',
+			required: true,
+			meta: {
+				width: 'full',
+				interface: 'textarea',
+			},
+		},
+		{
+			field: 'quality',
+			name: 'Quality',
+			type: 'string',
+			required: true,
+			meta: {
+				width: 'half',
+				interface: 'select-dropdown',
+				options: {
+					choices: [
+						{
+							text: 'Standard',
+							value: 'standard'
+						},
+						{
+							text: 'High',
+							value: 'hd'
+						},
+					]
+				}
+			}
+		},
+		{
+			field: 'size',
+			name: 'Size',
+			type: 'string',
+			required: true,
+			meta: {
+				width: 'half',
+				interface: 'select-dropdown',
+				options: {
+					choices: [
+						{
+							text: 'Square',
+							value: 'square'
+						},
+						{
+							text: 'Portrait',
+							value: 'portrait'
+						},
+						{
+							text: 'Landscape',
+							value: 'landscape'
+						},
+					]
+				}
+			}
+		},
+	],
+};
