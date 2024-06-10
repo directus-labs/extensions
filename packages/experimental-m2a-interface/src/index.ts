1a86283eaaceafb6c7d5e5eece23efa399766b04
formfcw <dev@formfcw.com>
2024-06-10 10:55:32 +0200

    add interface settings

--- packages/experimental-m2a-interface/src/index.ts
+++ packages/experimental-m2a-interface/src/index.ts
@@ -1,12 +1,41 @@
-import { defineInterface } from '@directus/extensions-sdk';
-import InterfaceComponent from './interface.vue';
+import { defineInterface } from "@directus/extensions-sdk";
+import InterfaceComponent from "./interface.vue";
 
 export default defineInterface({
-	id: 'directus-labs-experimental-m2a-interface',
-	name: 'M2A Button Matrix',
-	icon: 'box',
-	description: 'Add a matrix button selector to the built-in M2A interface',
-	component: InterfaceComponent,
-	options: null,
-	types: ['string'],
+    id: "directus-labs-experimental-m2a-interface",
+    name: "Builder (M2A) Button Matrix",
+    icon: "grid_view",
+    description: "Add a matrix button selector to the built-in M2A interface",
+    component: InterfaceComponent,
+    types: ["alias"],
+    localTypes: ["presentation"],
+    group: "presentation",
+    options: [
+        {
+            field: "target",
+            name: "Target Builder (M2A) field placement",
+            type: "string",
+            meta: {
+                width: "full",
+                interface: "select-dropdown",
+                options: {
+                    choices: [
+                        {
+                            text: "Above this field",
+                            value: "above",
+                            icon: "vertical_align_top",
+                        },
+                        {
+                            text: "Below this field",
+                            value: "below",
+                            icon: "vertical_align_bottom",
+                        },
+                    ],
+                },
+            },
+            schema: {
+                default_value: "below",
+            },
+        },
+    ],
 });
