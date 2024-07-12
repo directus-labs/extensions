805dfe4830065550ac2cee2934baf7ad897ba800
formfcw <dev@formfcw.com>
2024-07-12 12:47:31 +0200

    add interface
    adapt from video interface

---
+++ packages/audio-player-interface/src/types.ts
@@ -0,0 +1,6 @@
+export type AudioService = "external" | "directus";
+export type AudioSource = string;
+export type Audio =
+    | { service: AudioService; source: AudioSource }
+    | null
+    | undefined;
