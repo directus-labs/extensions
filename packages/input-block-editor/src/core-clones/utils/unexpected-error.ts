d161bb96c5b273be8662cc1fd8c11acbda3bf8a3
formfcw <dev@formfcw.com>
2024-08-14 08:45:12 +0200

    refactor input-block-editor interface

---
+++ packages/input-block-editor/src/core-clones/utils/unexpected-error.ts
@@ -0,0 +1,39 @@
+// CORE-CHANGE start
+// import type { RequestError } from "@/api";
+import type { RequestError } from "../types/api";
+// import { i18n } from '@/lang';
+import { useI18n } from "vue-i18n";
+// import { useNotificationsStore } from '@/stores/notifications';
+import { useStores } from "@directus/extensions-sdk";
+// import type { APIError } from "@/types/error";
+import type { APIError } from "../types/error";
+// CORE-CHANGE end
+
+let store: any;
+
+export function unexpectedError(error: unknown): void {
+    // CORE-CHANGE start
+    const { useNotificationsStore } = useStores();
+    // CORE-CHANGE end
+
+    if (!store) store = useNotificationsStore();
+
+    const code =
+        (error as RequestError).response?.data?.errors?.[0]?.extensions?.code ||
+        (error as APIError)?.extensions?.code ||
+        "UNKNOWN";
+
+    // eslint-disable-next-line no-console
+    console.warn(error);
+
+    store.add({
+        // CORE-CHANGE start
+        // title: i18n.global.t(`errors.${code}`),
+        title: useI18n().t(`errors.${code}`),
+        // CORE-CHANGE end
+        type: "error",
+        code,
+        dialog: true,
+        error,
+    });
+}
