19ed246776e7b350b075c00a9e7bd1e89af164f7
Tim Butterfield <124673267+timio23@users.noreply.github.com>
2024-08-29 21:26:33 +0100

    Addressed comments from PR

--- packages/table-view-panel/src/panel.vue
+++ packages/table-view-panel/src/panel.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="panel-table" :class="{ 'has-header': showHeader }">
 		<v-info type="warning" icon="warning" :center="true" v-if="!tableData && !isLoading" title="No Items"></v-info>
-		<v-info type="danger" icon="error" :center="true" v-else-if="hasError" :title="t('errors.INTERNAL_SERVER_ERROR')">{{ errorMessage }}</v-info>
+		<v-info type="warning" icon="warning" :center="true" v-else-if="fields.length == 0" title="No Fields Selected"></v-info>
+		<v-info type="danger" icon="error" :center="true" v-else-if="hasError" :title="errorResponse.title">{{ errorResponse.message }}</v-info>
 		<v-table
 			v-else
 				:sort="tableSort"
@@ -15,35 +16,41 @@
 				@click:row="editRow"
 				@update:sort="sortTrigger"
 			>
-				<template v-for="header in tableHeaders" :key="header.value" #[`item.${header.value}`]="{ item }">
-					<render-display
-						:value="get(item, header.value)"
-						:display="header.display"
-						:options="header.display_options"
-						:interface="header.interface"
-						:interface-options="header.interface_options"
-						:type="header.type"
-						:collection="header.collection"
-						:field="header.value"
-					/>
-				</template>
-			</v-table>
+			<template v-for="header in tableHeaders" :key="header.value" #[`item.${header.value}`]="{ item }">
+				<render-display
+					:value="get(item, header.value)"
+					:display="header.display"
+					:options="header.display_options"
+					:interface="header.interface"
+					:interface-options="header.interface_options"
+					:type="header.type"
+					:collection="header.collection"
+					:field="header.value"
+				/>
+			</template>
+		</v-table>
 
-			<drawer-item
-				:disabled="!canUpdate"
-				:active="viewItem !== null"
-				:collection="collection"
-				:primary-key="viewItem || '+'"
-				@input="saveChanges"
-				@update:active="cancelEdit"
-			/>
+		<drawer-item
+			:disabled="!canUpdate"
+			:active="viewItem !== null"
+			:collection="collection"
+			:primary-key="viewItem || '+'"
+			@input="saveChanges"
+			@update:active="cancelEdit"
+		/>
+		<v-dialog v-model="responseDialog" @esc="responseDialog = false">
+			<v-sheet class="panel-table-dialog">
+				<v-info type="danger" icon="error" :title="errorResponse.title">{{ errorResponse.message }}</v-info>
+				<v-button @click="responseDialog = false">Dismiss</v-button>
+			</v-sheet>
+		</v-dialog>
 	</div>
 </template>
 
 <script lang="ts">
 import { useI18n } from 'vue-i18n';
 import { useApi, useStores } from '@directus/extensions-sdk';
-import { defineComponent, ref, watch } from 'vue';
+import { defineComponent, onMounted, ref, watch } from 'vue';
 import { get, set } from 'lodash';
 
 export default defineComponent({
@@ -57,15 +64,14 @@ export default defineComponent({
 		},
 		collection: {
 			type: String,
-			default: '',
 		},
 		sort_field: {
 			type: String,
-			default: '',
+			default: null,
 		},
 		sort_direction: {
 			type: String,
-			default: 'sort_desc',
+			default: 'desc',
 		},
 		filter: {
 			type: Object,
@@ -83,12 +89,15 @@ export default defineComponent({
 	setup(props){
 		const { t } = useI18n();
 		const api = useApi();
-		const { useFieldsStore, usePermissionsStore } = useStores();
+		const { useFieldsStore, useRelationsStore, usePermissionsStore, useNotificationsStore } = useStores();
 		const fieldsStore = useFieldsStore();
+		const notificationStore = useNotificationsStore();
 		const { hasPermission } = usePermissionsStore();
 		const canRead = hasPermission(props.collection, 'read');
 		const canUpdate = hasPermission(props.collection, 'update');
 		const primaryKeyField = fieldsStore.getPrimaryKeyFieldForCollection(props.collection);
+		const sortField = ref(props.sort_field || primaryKeyField.field);
+		const sortDirection = ref(props.sort_direction || 'desc');
 
 		const tableData = ref<Array<Record<string, any>>>([]);
 		const tableHeaders = ref<Array<any>>([]);
@@ -97,37 +106,40 @@ export default defineComponent({
 		const tableCellWidth = ref<Record<string, number>>({});
 		const minCellWidth = 10;
 		const hasError = ref<boolean>(false);
-		const errorMessage = ref<String>('');
+		const errorResponse = ref<Record<string, String>>({
+			title: '',
+			message: '',
+		});
+		const responseDialog = ref<Boolean>(false);
 		const isLoading = ref<boolean>(true);
 		const viewItem = ref<string | null>(null);
 		const itemEdits = ref<Record<string, any>>({});
 
-		function fetchData(options: Record<string, any>): void {
+		async function fetchData(options: Record<string, any>): Promise<void> {
 			if(!options.sort_field || !options.sort_direction || !props.collection) return;
 			hasError.value = false;
 			isLoading.value = true;
 			const fields: string[] = [primaryKeyField.field, ...props.fields];
-			api.get(`/items/${props.collection}`,{
-				params: {
-					filter: props.filter,
-					fields: fields,
-					limit: props.limit ? props.limit : 10,
-					sort: sortKey(options.sort_field, options.sort_direction),
-				}
-			}).then((res) => {
+			try{
+				const response = await api.get(`/items/${props.collection}`,{
+					params: {
+						limit: props.limit ? props.limit : 10,
+						filter: props.filter,
+						fields: fields,
+						sort: await sortKey(options.sort_field, options.sort_direction),
+					}
+				});
+				
 				tableIDs.value = [];
-				tableData.value = res.data.data.map((item: Record<string, any>) => {
+				tableData.value = response.data.data.map((item: Record<string, any>) => {
 					let id = item[primaryKeyField.field];
 
 					if(tableIDs.value.includes(String(id))) return null;
 
 					props.fields.forEach((field_key) => {
-						console.log(`${field_key}:`);
-						console.log(item[field_key]);
 						let content_length = minCellWidth;
 						if(field_key.includes('.')){
 							let keys = field_key.split('.');
-							console.log(keys);
 							if(keys.length > 2){
 								let content: Record<string, any> = item;
 								for (let idx = 0; idx < keys.length; idx++) {
@@ -156,18 +168,40 @@ export default defineComponent({
 				}).filter((field: Object | null) => field !== null);
 
 				if(!options.refresh){
-					fetchHeaders();
+					await fetchHeaders();
+				} else {
+					props.fields.forEach((item, idx) => {
+						tableHeaders.value[idx].width = tableCellWidth.value[item] !== undefined && tableCellWidth.value[item] < 24 ? tableCellWidth.value[item] * 10 + (tableHeaders.value[idx].type == 'date' ? 30 : 20 ) : 390;
+					});
 				}
 				isLoading.value = false;
-			}).catch((error) => {
-				errorMessage.value = error.response.data.errors[0].message;
+			} catch(error) {
+				errorResponse.value.title = error.code || 'UNKNOWN';
+				errorResponse.value.message = error.message || t('errors.UNKNOWN');
 				hasError.value = true;
-			});
+			};
 		}
 
-		function fetchHeaders(){
+		function relationalCheck(fields: string){
+			if(!fields.includes(".")) return { collection: props.collection, fieldPath: fields };
+			const relationsStore = useRelationsStore();
+			const [field, ...path] = fields.split('.') as [string] & string[];
+			const relations = relationsStore.getRelationsForField(props.collection, field);
+
+			const relation = relations?.find((relation: Record<string, any>) => {
+				return relation.field === field || relation.meta?.one_field === field;
+			});
+
+			const relatedCollection = (relation === undefined ? props.collection : (relation.field === field && relation.related_collection !== props.collection ? relation.related_collection : relation.collection));
+
+			return { collection: relatedCollection, fieldPath: path.join('.') };
+		}
+
+		async function fetchHeaders(): Promise<void> {
 			tableHeaders.value = props.fields.map((field_key: string) => {
-				const field = fieldsStore.getField(props.collection, field_key);
+				if(!props.collection) return null;
+				const { collection, fieldPath } = relationalCheck(field_key);
+				const field = fieldsStore.getField(collection, fieldPath);
 				if(!field) return null;
 				if(field.type == 'timestamp' && field.meta.display_options.relative) tableCellWidth.value[field_key] = 14;
 
@@ -187,58 +221,64 @@ export default defineComponent({
 			}).filter((field: Object | string | null) => field !== null);
 		}
 
-		function sortKey(sort_field: string, sort_direction: string) {
-			if(!sort_field) return;
+		async function sortKey(sort_field: string, sort_direction: string): Promise<Array<string>> {
+			if(!sort_field) return [primaryKeyField.field];
 			tableSort.value = {
 				by: sort_field,
-				desc: sort_direction == 'sort_desc'
+				desc: sort_direction == 'desc'
 			};
-			return [`${sort_direction != 'sort_asc'?'-':''}${sort_field}`];
+			return [`${sort_direction != 'asc'?'-':''}${sort_field}`];
 		}
 
-		function editRow(e: any) {
+		async function editRow(e: any) {
 			viewItem.value = e.item[primaryKeyField.field];
 		}
 
-		function cancelEdit(e: any){
+		async function cancelEdit(){
 			viewItem.value = null;
 			itemEdits.value = {};
 		}
 
-		async function saveChanges(item: Record<string, any>) {
+		async function saveChanges(item: Record<string, any>): Promise<void> {
 			try {
-				await api.patch(`/items/${(props.collection)}/${viewItem.value}`, item);
+				await api.patch(`/itemss/${(props.collection)}/${viewItem.value}`, item);
+				notificationStore.add({
+					title: t('item_update_success', 1),
+				});
+				fetchData({
+					sort_field: sortField.value,
+					sort_direction: sortDirection.value,
+					refresh: false,
+				});
 			} catch (error) {
-				console.log(error);
+				errorResponse.value.title = error.code || 'UNKNOWN';
+				errorResponse.value.message = error.message || t('errors.UNKNOWN');
+				responseDialog.value = true;
 			}
-			fetchData({
-				sort_field: props.sort_field,
-				sort_direction: props.sort_direction,
-				refresh: true
-			});
 		}
 
-		function sortTrigger(sort: any){
+		async function sortTrigger(sort: any): Promise<void> {
 			if(!sort){
 				fetchData({
-					sort_field: props.sort_field,
-					sort_direction: props.sort_direction,
+					sort_field: sortField.value,
+					sort_direction: sortDirection.value,
 					refresh: true,
 				});
 			} else {
 				fetchData({
 					sort_field: sort.by,
-					sort_direction: (sort.desc ? 'sort_desc' : 'sort_asc'),
+					sort_direction: (sort.desc ? 'desc' : 'asc'),
 					refresh: true,
 				});
 			}
 		}
 
-		// On load
-		fetchData({
-			sort_field: props.sort_field,
-			sort_direction: props.sort_direction,
-			refresh: tableHeaders.value.length > 0
+		onMounted(() => {
+			fetchData({
+				sort_field: sortField.value,
+				sort_direction: sortDirection.value,
+				refresh: tableHeaders.value.length > 0
+			});
 		});
 
 		// Changes to panel
@@ -253,8 +293,8 @@ export default defineComponent({
 			],
 			() => {
 				fetchData({
-					sort_field: props.sort_field,
-					sort_direction: props.sort_direction,
+					sort_field: props.sort_field || primaryKeyField.field,
+					sort_direction: props.sort_direction || 'desc',
 					refresh: false
 				});
 			},
@@ -268,7 +308,8 @@ export default defineComponent({
 			
 			// Errors
 			hasError,
-			errorMessage,
+			errorResponse,
+			responseDialog,
 
 			// Permissions
 			canRead,
@@ -318,16 +359,18 @@ export default defineComponent({
 	overflow: scroll;
 }
 
-.panel-table table {
-
-}
-
 .panel-table table thead tr {
 	position: sticky;
 	top: 0;
 }
 
-.panel-table table tbody {
+.panel-table-dialog.v-sheet {
+	text-align: center;
+	min-width: 300px;
+	width: 400px;
+}
 
+.panel-table-dialog .v-info {
+	margin-bottom: 1em;
 }
 </style>
