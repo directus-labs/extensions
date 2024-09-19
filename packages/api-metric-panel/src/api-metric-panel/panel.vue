6973abc5cf3788db888e3dfb9f38ce9b49e3d8e7
Dominic <dominic.marcelino@utomic.media>
2024-09-19 22:39:56 +0200

    FEAT: add panel rendering

--- packages/api-metric-panel/src/api-metric-panel/panel.vue
+++ packages/api-metric-panel/src/api-metric-panel/panel.vue
@@ -1,64 +1,19 @@
 <script setup lang="ts">
 import type { CSSProperties } from 'vue';
+import { computed, ref, onMounted, onUpdated, onBeforeUnmount } from 'vue';
+import type { Style, Notation, Unit } from './utils/format-number';
+import { useAutoFontFit } from './composables/use-auto-fit-text';
+import { formatNumber } from './utils/format-number';
+import { useI18n } from 'vue-i18n';
 
-const SIMPLE_UNITS = [
-	'acre',
-	'bit',
-	'byte',
-	'celsius',
-	'centimeter',
-	'day',
-	'degree',
-	'fahrenheit',
-	'fluid-ounce',
-	'foot',
-	'gallon',
-	'gigabit',
-	'gigabyte',
-	'gram',
-	'hectare',
-	'hour',
-	'inch',
-	'kilobit',
-	'kilobyte',
-	'kilogram',
-	'kilometer',
-	'liter',
-	'megabit',
-	'megabyte',
-	'meter',
-	'mile',
-	'mile-scandinavian',
-	'millimeter',
-	'milliliter',
-	'millisecond',
-	'minute',
-	'month',
-	'ounce',
-	'percent',
-	'petabyte',
-	'pound',
-	'second',
-	'stone',
-	'terabit',
-	'terabyte',
-	'week',
-	'yard',
-	'year',
-] as const;
-
-type SimpleUnit = (typeof SIMPLE_UNITS)[number];
 
 type Header = {
 	header: string;
 	value: string;
 };
 
-type Style = 'decimal' | 'currency' | 'percent' | 'unit';
+type ConditionalFormatting = Record<string, any>;
 
-type Notation = 'standard' | 'scientific' | 'engineering' | 'compact';
-
-type Unit = SimpleUnit | `${SimpleUnit}-per-${SimpleUnit}`; // Compound unit type
 
 interface Props {
 	showHeader?: boolean;
@@ -75,12 +30,13 @@ interface Props {
 	unit?: Unit;
 	minimumFractionDigits?: number;
 	maximumFractionDigits?: number;
-	conditionalFormatting?: Record<string, any>[] | null;
+	conditionalFormatting?: ConditionalFormatting[] | null;
 	textAlign?: CSSProperties['text-align'];
 	fontWeight?: number | undefined;
 	fontStyle?: string | undefined;
 	fontSize?: string;
 	font?: 'sans-serif' | 'serif' | 'monospace';
+	metric?: number | string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -102,23 +58,171 @@ const props = withDefaults(defineProps<Props>(), {
 	fontStyle: 'normal',
 	fontSize: 'auto',
 	font: 'sans-serif',
+	metric: undefined,
+});
+
+const { locale } = useI18n();
+
+const labelContainer = ref<HTMLDivElement | null>(null);
+const labelText = ref<HTMLParagraphElement | null>(null);
+
+const { adjustFontSize } = useAutoFontFit(labelContainer, labelText);
+
+let resizeObserver: ResizeObserver | null = null;
+
+function adjustPadding() {
+	const container = labelContainer.value;
+	if (!container) return;
+
+	const paddingWidth = container.offsetWidth * 0.05;
+	const paddingHeight = container.offsetHeight * 0.05;
+
+	const padding = Math.round(Math.max(8, Math.min(paddingWidth, paddingHeight)));
+
+	if (props.showHeader == true) {
+		container.style.padding = '0px 12px 12px 12px';
+	} else {
+		container.style.padding = `${padding}px`;
+	}
+
+	return;
+}
+
+function unmountResizeObserver() {
+	if (resizeObserver) {
+		resizeObserver.disconnect();
+		resizeObserver = null;
+	}
+}
+
+async function updateFit() {
+	if (props.fontSize !== 'auto' || !props.metric) {
+		unmountResizeObserver();
+		return;
+	}
+
+	await document.fonts.ready;
+	adjustPadding();
+	adjustFontSize();
+
+	if (!resizeObserver) {
+		const container = labelContainer.value;
+		if (!container) return;
+
+		// Create a ResizeObserver to watch for changes in the container's dimensions
+		resizeObserver = new ResizeObserver(() => {
+			updateFit();
+		});
+
+		resizeObserver.observe(container);
+	}
+
+	adjustFontSize();
+}
+
+onMounted(() => {
+	updateFit();
+});
+
+onUpdated(() => {
+	updateFit();
+});
+
+onBeforeUnmount(() => {
+	unmountResizeObserver();
+});
+
+const metric = computed(() => {
+	if (!props.metric) return null;
+
+	if (props.fieldIsNumber && !isNaN(Number(props.metric)) ) {
+		return Number(props.metric);
+	}
+
+	return props.metric;
+});
+
+function displayValue(value: number | string) {
+	if (value === null || value === undefined) {
+		return 0;
+	}
+
+	return formatNumber(Number(value), locale.value, {
+		notation: props.notation,
+		style: props.numberStyle,
+		unit: props.unit,
+		minimumFractionDigits: props.minimumFractionDigits,
+		maximumFractionDigits: props.maximumFractionDigits,
+		currency: props.numberStyle === 'currency' ? String(props.unit) : undefined,
+	});
+}
+
+const color = computed(() => {
+	if (!metric.value) return null;
+
+	let matchingFormat = null as ConditionalFormatting | null;
+
+	for (const format of props.conditionalFormatting || []) {
+		if (matchesOperator(format)) {
+			matchingFormat = format;
+		}
+	}
+
+	return matchingFormat?.color || 'var(--theme--primary)';
+
+	function matchesOperator(format: Record<string, any>) {
+		if (typeof metric.value === 'string') {
+			const value = metric.value;
+			const compareValue = format.value ?? '';
+
+			switch (format.operator || '>=') {
+				case '=':
+					return value === compareValue;
+				case '!=':
+					return value !== compareValue;
+			}
+		} else {
+			const value = Number(metric.value);
+			const compareValue = Number(format.value ?? 0);
+
+			switch (format.operator || '>=') {
+				case '=':
+					return value === compareValue;
+				case '!=':
+					return value !== compareValue;
+				case '>':
+					return value > compareValue;
+				case '>=':
+					return value >= compareValue;
+				case '<':
+					return value < compareValue;
+				case '<=':
+					return value < compareValue;
+			}
+		}
+
+		return false;
+	}
 });
 </script>
 
 
 <template>
 	<div ref="labelContainer" class="api-metric type-title selectable" :class="[font, { 'has-header': showHeader }]">
-		<p v-if="!props.url">
-			Missing URL
-		</p>
 		<p
-			v-else
 			ref="labelText"
 			class="api-metric-text"
-			:style="{ fontWeight, textAlign, fontStyle, fontSize: fontSize !== 'auto' ? fontSize : undefined }"
+			:style="{ color, fontWeight, textAlign, fontStyle, fontSize: fontSize !== 'auto' ? fontSize : undefined }"
 		>
-			{{ prefix }}
-			{{ suffix }}
+			<template v-if="!props.url">
+				Missing URL
+			</template>
+
+			<template v-else>
+				{{ prefix }}
+				{{ displayValue(metric) }}
+				{{ suffix }}
+			</template>
 		</p>
 	</div>
 
--- packages/api-metric-panel/src/api-metric-panel/shims.d.ts
+++ packages/api-metric-panel/src/api-metric-panel/shims.d.ts
@@ -3,3 +3,11 @@ declare module '*.vue' {
 	const component: DefineComponent<{}, {}, any>;
 	export default component;
 }
+
+
+declare module 'vue' {
+  import { DefineComponent } from 'vue'
+  const Vue: DefineComponent<{}, {}, any>
+  export default Vue
+  export * from '@vue/runtime-dom'
+}
