6c4d3c27aa580eea674ccdbe35f05a171cbf0ed1
Dominic <dominic.marcelino@utomic.media>
2024-11-20 13:06:08 +0100

    FEAT: render map

--- packages/address-completion-interface/src/interface.vue
+++ packages/address-completion-interface/src/interface.vue
@@ -52,32 +52,41 @@ const results = ref<AutocompleteLocation[]>([]);
 const sessionToken = ref<google.maps.places.AutocompleteSessionToken | null>(null);
 const searchInput = ref<string | null>(null);
 const selectedPlaceId = ref<string | null>(null);
+const mapContainer = ref<HTMLElement | null>(null);
+const searchContainer = ref<HTMLElement | null>(null);
+
 const isDark = document.body.classList.contains('dark');
 const lang = getCurrentLanguage();
 
 let places: google.maps.PlacesLibrary;
+let maps: google.maps.MapsLibrary;
 
 
 onMounted( async () => {
 	const loader = new Loader({
 		apiKey: props.apiKeyGMaps,
-		libraries: ['places'],
+		libraries: ['places', 'maps'],
 		version: 'weekly',
 	});
 
 	places = await loader.importLibrary("places");
+	maps = await loader.importLibrary("maps");
 
 	setNewSessionToken();
+	initMap();
 });
+
 
 function setNewSessionToken() {
 	sessionToken.value = new places.AutocompleteSessionToken();
 }
 
+
 async function onInput(value: string) {
 	searchInput.value = value;
 	makeAutocompleteRequest();
 }
+
 
 async function makeAutocompleteRequest() {
 	if (!searchInput.value) {
@@ -116,55 +125,84 @@ async function onPlaceSelected(location: AutocompleteLocation) {
 	selectedPlaceId.value = location.placeId;
 	setNewSessionToken();
 }
-	
+
+// TODO: in case that a locations exist, init it with it
+function initMap() {
+  if (!mapContainer.value) {
+    return;
+  }
+
+  const map = new maps.Map(mapContainer.value, {
+    center: { lat: 0, lng: 0 },
+		zoom: 1,
+		streetViewControl: false,
+		mapTypeControlOptions: {
+			style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+			position: google.maps.ControlPosition.TOP_RIGHT,
+		},
+
+	});
+
+	if (searchContainer.value) {
+		console.log('Push search container');
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchContainer.value!);
+	}
+}
 </script>
 
 
 <template>
-	<v-menu attached :disabled="disabled">
-		<template #activator="{ activate }">
-			<v-input
-				:placeholder="t('search')"
-				:model-value="searchInput"
-				:disabled="disabled"
-				@update:model-value="onInput"
-				@focus="activate"
-			>
-				<template v-if="iconLeft" #prepend>
-					<v-icon :name="iconLeft" />
-				</template>
-				
-				<template v-if="iconRight" #append>
-					<v-icon :name="iconRight" />
-				</template>
-			</v-input>
-		</template>
+	<div ref="searchContainer" class="search-container">
+		<v-menu attached :disabled="disabled">
+			<template #activator="{ activate }">
+				<v-input
+					:placeholder="t('search')"
+					:model-value="searchInput"
+					:disabled="disabled"
+					:small="props.displayMap ? true : false"
+					@update:model-value="onInput"
+					@focus="activate"
+				>
+					<template v-if="iconLeft" #prepend>
+						<v-icon :name="iconLeft" />
+					</template>
+					
+					<template v-if="iconRight" #append>
+						<v-icon :name="iconRight" />
+					</template>
+				</v-input>
+			</template>
 
-		<v-list v-if="results.length > 0">
-			<v-list-item
-				v-for="result of results"
-				:key="result.placeId"
-				:class="selectedPlaceId === result.placeId ? 'selected' : ''"
-				@click="() => onPlaceSelected(result)"
-			>
-				{{ result.text }}
-			</v-list-item>
+			<v-list v-if="results.length > 0">
+				<v-list-item
+					v-for="result of results"
+					:key="result.placeId"
+					:class="selectedPlaceId === result.placeId ? 'selected' : ''"
+					@click="() => onPlaceSelected(result)"
+				>
+					{{ result.text }}
+				</v-list-item>
 
-			<!-- 
-				If we do not display the map, we need to include the google logo in here
-			 	@see https://developers.google.com/maps/documentation/javascript/place-autocomplete-data
-				@see https://developers.google.com/maps/documentation/javascript/policies#logo
-			-->
-			<v-list-item 
-				v-if="!props.displayMap"
-				:clickable="false" 
-				:disabled="true" 
-				class="google-logo"
-			>
-				<img :src="isDark ? googleLogoDark : googleLogo" alt="Google Logo" />
-			</v-list-item>
-		</v-list>
-	</v-menu>
+				<!-- 
+					If we do not display the map, we need to include the google logo in here
+					@see https://developers.google.com/maps/documentation/javascript/place-autocomplete-data
+					@see https://developers.google.com/maps/documentation/javascript/policies#logo
+				-->
+				<v-list-item 
+					v-if="!props.displayMap"
+					:clickable="false" 
+					:disabled="true" 
+					class="google-logo"
+				>
+					<img :src="isDark ? googleLogoDark : googleLogo" alt="Google Logo" />
+				</v-list-item>
+			</v-list>
+		</v-menu>
+	</div>
+
+	<div v-if="props.displayMap">
+		<div ref="mapContainer" class="map-container"></div>
+	</div>
 </template>
 
 
@@ -175,7 +213,36 @@ async function onPlaceSelected(location: AutocompleteLocation) {
 		&:hover {
 			background-color: var(--v-list-item-background-color-active, var(--v-list-background-color-active, var(--theme--background-normal)));
 		}
+
+		&.google-logo {
+			&:hover {
+				background-color: transparent;
+			}
+
+			img {
+				height: 1.25rem;
+			}
+		}
 	}
+}
+
+.map-container {
+	height: 500px;
+	width: 100%;
+
+	.search-container {
+		margin: 10px;
+		width: 80%;
+	}
+}
+
+.search-control {
+  margin: 10px;
+  min-width: 300px;
+
+  :deep(.v-menu) {
+    z-index: 1000;
+  }
 }
 
 </style>
