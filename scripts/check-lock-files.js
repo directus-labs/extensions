f086ce8d1ee50a3c84352cc7b69dfeeda0b99d4e
Mike Elsmore <mike@directus.io>
2025-02-05 11:52:53 -0700

    Cleanup lockfile check

--- scripts/check-lock-files.js
+++ scripts/check-lock-files.js
@@ -1,7 +1,9 @@
 const fs = require('node:fs');
 const process = require('node:process');
 
-if (fs.readFileSync('./package-lock.json')) {
+console.log(`Checking ${process.cwd()}`);
+
+if (fs.existsSync('./package-lock.json')) {
 	console.error(
 		'package-lock.json is unecessary due to pnpm workspace, please remove',
 	);
@@ -9,7 +11,7 @@ if (fs.readFileSync('./package-lock.json')) {
 	process.exit(1);
 }
 
-if (fs.readFileSync('./pnpm-lock.yaml')) {
+if (fs.existsSync('./pnpm-lock.yaml')) {
 	console.error(
 		'pnpm-lock.yaml is unecessary due to pnpm workspace, please remove',
 	);
