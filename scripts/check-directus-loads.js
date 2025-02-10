c4cb2ca461ddd104df4ff31a428071ee0a4a3a63
Mike Elsmore <mike@directus.io>
2025-02-10 13:55:37 -0700

    Checks if docker instance of directus will execute

---
+++ scripts/check-directus-loads.js
@@ -0,0 +1,41 @@
+import Buffer from 'node:buffer';
+import fs from 'node:fs';
+import process from 'node:process';
+import { execa, execaSync } from 'execa';
+
+const DIRECTUS_VERSION = process.argv.slice(2)[0] || 'latest';
+
+const { name } = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
+const packageName = name.split('/')[1];
+
+console.log(`Checking package ${name}`);
+
+async function dockerRun(image, name, path, version) {
+	console.log(`Run ${image}`);
+
+	try {
+		const readable = execa({ shell: true })`docker run --rm -v ./packages/${name}:${path} --name ${name}.${version} ${image}`.readable();
+
+		readable.on('data', (data) => {
+			if (data.toString().includes('App [directus:0] online')) {
+				try {
+					readable.destroy();
+					console.log(`Run ${image} success`);
+					process.exit(0);
+				}
+				catch (error) {
+					readable.destroy();
+					console.error(error);
+				}
+			}
+		});
+	}
+	catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+}
+
+const image = `directus/directus:${DIRECTUS_VERSION}`;
+await dockerRun(image, packageName, process.cwd(), DIRECTUS_VERSION);
