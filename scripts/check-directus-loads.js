c3939e750fa08e976e5a8197f1bc19a3c157151b
Mike Elsmore <mike@directus.io>
2025-02-11 17:56:02 -0700

    Add a force stop and remove of containers

--- scripts/check-directus-loads.js
+++ scripts/check-directus-loads.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import process from 'node:process';
-import { execa } from 'execa';
+import { execa, execaSync } from 'execa';
 
 const DIRECTUS_VERSION = process.argv.slice(2)[0] || 'latest';
 
@@ -10,38 +10,49 @@ const packageName = name.split('/')[1];
 
 console.log(`Checking package ${name}`);
 
+async function dockerRemove(name) {
+	console.log(`Stopping ${name}`);
+	return await execaSync({ shell: true })`docker rm --force ${name}`;
+}
+
 async function dockerRun(image, name, path, version) {
 	console.log(`Run ${image}`);
 
+	const containerName = `${name}.${version}`;
+
 	try {
-		const readable = execa({ shell: true })`docker run --rm -v ./packages/${name}:${path} --name ${name}.${version} ${image}`.readable();
+		const readable = execa({ shell: true })`docker run --rm -v ./packages/${name}:${path} --name ${containerName} ${image}`.readable();
 
 		let success = false;
 
-		readable.on('data', (data) => {
+		readable.on('data', async (data) => {
 			if (data.toString().includes('App [directus:0] online')) {
 				try {
 					readable.destroy();
 					console.log(`Run ${image} success`);
 					success = true;
+					await dockerRemove(containerName);
 					process.exit(0);
 				}
 				catch (error) {
 					readable.destroy();
+					await dockerRemove(containerName);
 					console.error(error);
 				}
 			}
 		});
 
-		setTimeout(() => {
+		setTimeout(async () => {
 			if (!success) {
 				console.error(`Run ${image} fail - timeout`);
+				await dockerRemove(containerName);
 				process.exit(1);
 			}
 		}, 30000);
 	}
 	catch (error) {
 		console.error(error);
+		await dockerRemove(containerName);
 		process.exit(1);
 	}
 }
