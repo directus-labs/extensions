2a735df9c0353a6827a47149946962e64ebfe068
Mike Elsmore <mike@directus.io>
2025-03-07 13:51:53 +0000

    Automate the update of Issue Templates (#167)

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -30,7 +30,20 @@ const formattedPackages = packages.map((packageDir) => {
 	};
 });
 
-const readmeTemplate = fs.readFileSync('scripts/templates/readme.md. mustache').toString();
+// README
+const readmeTemplate = fs.readFileSync('scripts/templates/readme.md.mustache').toString();
 const readmeOutput = Mustache.render(readmeTemplate, { extensions: formattedPackages });
 
 fs.writeFileSync('./readme.md', readmeOutput);
+
+// Bug Report
+const bugReportTemplate = fs.readFileSync('scripts/templates/bug-report.yml.mustache').toString();
+const bugReportOutput = Mustache.render(bugReportTemplate, { extensions: formattedPackages });
+
+fs.writeFileSync('.github/ISSUE_TEMPLATE/bug-report.yml', bugReportOutput);
+
+// Enhancement
+const enhancementTemplate = fs.readFileSync('scripts/templates/enhancement.yml.mustache').toString();
+const enhancementOutput = Mustache.render(enhancementTemplate, { extensions: formattedPackages });
+
+fs.writeFileSync('.github/ISSUE_TEMPLATE/enhancement.yml', enhancementOutput);
