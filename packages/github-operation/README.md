41b0d3196b48072fc284745b781dda339f3f6a2d
Dominic <dominic.marcelino@utomic.media>
2025-03-17 15:54:25 +0100

    FEAT: add github operation

--- packages/github-operation/README.md
+++ packages/github-operation/README.md
@@ -1,1 +1,49 @@
 # GitHub Operation
+An operation that lets Flows trigger GitHub Actions using the repository_dispatch event.
+
+[](https://raw.githubusercontent.com/directus-labs/extensions/main/packages/github-operation/docs/options.png)
+
+## Configuration
+- **Owner**: GitHub user or organization name
+- **Repository**: Target repository name
+- **Access Token**: GitHub token with appropriate permissions
+- **Event type**: Custom event name for workflow filtering
+- **Client Payload** : JSON data passed to your workflow
+
+## Flow-Authentication
+> [!IMPORTANT]
+> Directus Flows can be triggered publicly by default. Add authentication operations if needed.
+
+## Github Setup
+1. Create a [GitHub Access Token](https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-a-repository-dispatch-event)
+2. Add a workflow to your repository
+3. Create your directus flow
+
+### Example Workflow
+```yml
+#FILE: .github/workflows/example.yml
+
+name: Directus Dispatched Action
+
+on:
+  repository_dispatch:
+    # Specify the event types on which the flow should be triggered
+    types: [example_event]
+
+jobs:
+  example:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v4
+
+      # Echo the event name
+      - env:
+          ACTION: ${{ github.event.action }}
+        run: echo $ACTION
+
+      # Echo the Payload
+      - env:
+          PAYLOAD: ${{ toJSON(github.event.client_payload) }}
+        run: echo $PAYLOAD
+```
--- packages/github-operation/src/api.ts
+++ packages/github-operation/src/api.ts
@@ -1,12 +1,58 @@
+/// <reference types="@directus/extensions/api.d.ts" />
 import { defineOperationApi } from '@directus/extensions-sdk';
+import { InvalidPayloadError } from '@directus/errors';
+import { request, log } from 'directus:api';
 
-type Options = {
-	text: string;
+type GithubOperationOptions = {
+	owner: string;
+	repo: string;
+	accessToken: string;
+	eventType: string;
+	clientPayload: JSON;
 };
 
-export default defineOperationApi<Options>({
+type RequestBody = {
+	event_type: string;
+	client_payload?: JSON;
+}
+
+export default defineOperationApi<GithubOperationOptions>({
 	id: 'directus-labs-github-operation',
-	handler: ({ text }) => {
-		console.log(text);
+	handler: async (options) => {
+		const requiredFields: (keyof GithubOperationOptions)[] = ['owner', 'repo', 'accessToken', 'eventType'];
+
+		for (const field of requiredFields) {
+			if (!options[field]) {
+				throw new InvalidPayloadError({ reason: `Missing required field: ${field}` });
+			}
+		}
+
+		const requestBody: RequestBody = {
+			event_type: options.eventType,
+		};
+
+		if (options.clientPayload) {
+			requestBody.client_payload = options.clientPayload;
+		}
+
+		try {
+			const _response = await request(`https://api.github.com/repos/${options.owner}/${options.repo}/dispatches`, {
+				method: 'POST',
+				headers: {
+					'Authorization': `Bearer ${options.accessToken}`,
+					'Accept': 'application/vnd.github+json',
+					'X-GitHub-Api-Version': '2022-11-28',
+				},
+				body: JSON.stringify(requestBody),
+			});
+
+			return { status: 204 };
+		} catch (error: any) {
+			const response = JSON.parse(error.response);
+			const errorString = JSON.stringify(response.data) || 'Github Request failed, please check your configuration'
+			log(errorString);
+
+			throw new Error(errorString);
+		}
 	},
 });
