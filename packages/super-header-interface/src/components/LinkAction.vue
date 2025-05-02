ac1b050872504f7cfbeb665876a1df3abc728c93
bryantgillespie <hey@bryantgillespie.com>
2025-05-02 09:31:40 -0400

    add logic for internal links

--- packages/super-header-interface/src/components/LinkAction.vue
+++ packages/super-header-interface/src/components/LinkAction.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import BaseAction from './BaseAction.vue';
 
 interface LinkActionProps {
@@ -8,9 +9,38 @@ interface LinkActionProps {
 	url: string;
 }
 
-defineProps<LinkActionProps>();
+const props = defineProps<LinkActionProps>();
+
+const link = computed(() => {
+	const isInternal = props.url.startsWith('/') || props.url.startsWith('admin');
+
+	let processedUrl = props.url;
+
+	if (isInternal) {
+		// Remove any potential 'admin' prefixes to prevent duplication
+		const adminPrefixes = ['/admin/', '/admin', 'admin/', 'admin'];
+
+		for (const prefix of adminPrefixes) {
+			if (processedUrl.startsWith(prefix)) {
+				processedUrl = processedUrl.slice(prefix.length);
+				break;
+			}
+		}
+
+		// Ensure the processed URL has a leading slash
+		processedUrl = processedUrl.startsWith('/') ? processedUrl : `/${processedUrl}`;
+	}
+
+	return {
+		label: props.label,
+		icon: props.icon,
+		type: props.type,
+		to: isInternal ? processedUrl : undefined,
+		href: isInternal ? undefined : props.url,
+	};
+});
 </script>
 
 <template>
-	<BaseAction :label="label" :icon="icon" :type="type" :href="url" />
+	<BaseAction :label="link.label" :icon="link.icon" :type="link.type" :to="link.to" :href="link.href" />
 </template>
