7095d2c966cef1d6fc7eaa88f675a6230544b7fa
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2025-05-19 16:19:06 -0400

    Do not transform array of values to obj array

--- packages/collaborative-editing/src/interface/utils/parse-update.ts
+++ packages/collaborative-editing/src/interface/utils/parse-update.ts
@@ -4,11 +4,12 @@ import { UNDEFINED_VALUE } from '../../constants';
 
 export function parseUpdate<T>(message: T) {
 	const parsed: Record<string, unknown> = {};
+
 	for (const [key, value] of _.entries(message as object)) {
 		if (isObject(value)) {
 			parsed[key] = parseUpdate(value as Record<string, unknown>);
 		} else if (Array.isArray(value)) {
-			parsed[key] = value.map((v) => parseUpdate(v));
+			parsed[key] = value.map((v) => (isObject(v) ? parseUpdate(v) : v));
 		} else if (value === UNDEFINED_VALUE) {
 			parsed[key] = undefined;
 		} else {
