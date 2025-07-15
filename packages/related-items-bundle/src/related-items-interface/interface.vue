e2b4f20c5ea079c3bd65dba90950a85d41506b2f
Tim Butterfield <tim.butterfield@me.com>
2025-07-15 14:24:07 +0100

    Related items module (#191)
    * initial upload
    
    * Update pnpm-lock.yaml
    
    * added API reference to README
    
    * moved admin check to index
    
    * removed comments
    
    * added keywords and authors
    
    * updated Directus minimum version
    
    * hide Label on interface
    
    * settings field full width
    
    * removed special parameter
    
    * datetime improvements on mobile
    
    * graceful errors for hook
    
    * added support for junction tables
    
    * notification on item update and stop self editing
    
    * special parameters required
    
    * eslint changes
    
    * moved author and contributors - linting
    
    * another linting error
    
    * try catch on endpoint
    
    * Improve icon color
    
    * Improve v-chip hover styling
    
    * nullified related items ID bug fix
    
    * Added label to template
    
    * Show notice when creating new item
    
    * Make label persistant
    
    * fixed text alignment
    
    * remove duplicate items
    
    * hydrate fields after saving settings
    
    * added systen-data dependancy
    
    * linting fixes
    
    * Update pnpm-lock.yaml
    
    * fixed ghosting on interface
    
    * pixel adjustment to collection label
    
    * prevent editing if already present on item
    
    * Type improvements
    
    * added shared resource
    
    * added reactivity to visible related fields
    
    * removed @directus/eslint-config dependancy
    
    * Update pnpm-lock.yaml
    
    * filter a2m duplicates + m2o staged edits
    
    * disable all related items while read-only
    
    * consistant click actions
    
    * performance improvements with activity
    
    * made M2O read-only if visible on item as O2M
    
    * ensure that the item link always appears, even when disabled
    
    ---------
    
    Co-authored-by: Mike Elsmore <mike@directus.io>
    Co-authored-by: formfcw <dev@formfcw.com>
    Co-authored-by: Alex Gaillard <alex@directus.io>

---
+++ packages/related-items-bundle/src/related-items-interface/interface.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { toRefs } from 'vue';
+import RelatedItemsList from './related-items-list.vue';
+
+const props = withDefaults(
+	defineProps<{
+		disabled?: boolean;
+		collection: string;
+		primaryKey: number | string;
+		loading: boolean;
+	}>(),
+	{
+		disabled: false,
+	},
+);
+
+const { collection, primaryKey, loading } = toRefs(props);
+</script>
+
+<template>
+	<div v-if="!loading" class="related-items-list">
+		<RelatedItemsList
+			:primary-key="primaryKey"
+			:collection="collection"
+			:disabled
+		/>
+	</div>
+</template>
---
+++ packages/related-items-bundle/src/related-items-module/index.ts
@@ -0,0 +1,16 @@
+import { defineModule } from '@directus/extensions-sdk';
+import { injectRelatedItemsField } from './utils/inject-related-items-field';
+
+export default defineModule({
+	id: 'related-items',
+	hidden: true,
+	name: 'Related Items',
+	icon: 'hub',
+	routes: [],
+	preRegisterCheck(user) {
+		const admin = user.admin_access;
+		if (!admin) return false;
+		injectRelatedItemsField();
+		return true;
+	},
+});
