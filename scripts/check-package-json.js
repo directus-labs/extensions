c7dc13914603528e57989088647a436e21c5b71b
ian <licitdev@gmail.com>
2025-12-24 13:47:50 +0800

    Add repository directory check

--- scripts/check-package-json.js
+++ scripts/check-package-json.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs');
+const path = require('node:path');
 const process = require('node:process');
 
 const { name, repository } = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
@@ -10,5 +11,12 @@ if (!name.startsWith('@directus-labs/')) {
 
 if (repository?.url !== 'https://github.com/directus-labs/extensions.git') {
 	console.error('repository.url is not correct');
+	process.exit(1);
+}
+
+const expectedDirectory = path.relative(path.join(__dirname, '..'), process.cwd());
+
+if (repository?.directory !== expectedDirectory) {
+	console.error(`Expected directory to be "${expectedDirectory}" instead of "${repository?.directory}"`);
 	process.exit(1);
 }
