1b385ae4e2ad89c7e6b9655e250eaa4687061abf
ian <licitdev@gmail.com>
2025-12-24 13:24:18 +0800

    Add repository url check

--- scripts/check-package-json.js
+++ scripts/check-package-json.js
@@ -1,9 +1,14 @@
 const fs = require('node:fs');
 const process = require('node:process');
 
-const { name } = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const { name, repository } = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 if (!name.startsWith('@directus-labs/')) {
 	console.error('Not part of @directus-labs npm org');
+	process.exit(1);
+}
+
+if (repository?.url !== 'https://github.com/directus-labs/extensions.git') {
+	console.error('repository.url is not correct');
 	process.exit(1);
 }
