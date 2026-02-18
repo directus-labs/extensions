fc1597c4dccb4f02db72858940321890d3b7b80c
bryantgillespie <hey@bryantgillespie.com>
2026-02-18 13:44:06 -0500

    fix eslint config with lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,25 @@
-import directusConfig from "@directus/eslint-config";
+import directusConfig from '@directus/eslint-config';
 
 export default [
-  ...directusConfig,
-  {
-    ignores: ["**/*.md", "**/shims.d.ts", "**/shim.d.ts", "archived/**"],
-  },
-  {
-    rules: {
-      // Temporary disabled, will be resolved step by step
-      "unicorn/no-array-callback-reference": "off",
-      "unicorn/no-await-expression-member": "off",
-      "unicorn/consistent-function-scoping": "off",
-      "unicorn/no-array-for-each": "off",
-      "unicorn/prefer-spread": "off",
-      "unicorn/prefer-module": "off",
-    },
-  },
-  {
-    files: ["**/*.vue"],
-    rules: {
-      "vue/valid-v-slot": "warn",
-    },
-  },
+	...directusConfig,
+	{
+		ignores: ['**/*.md', '**/shims.d.ts', '**/shim.d.ts', 'archived/**'],
+	},
+	{
+		rules: {
+			// Temporary disabled, will be resolved step by step
+			'unicorn/no-array-callback-reference': 'off',
+			'unicorn/no-await-expression-member': 'off',
+			'unicorn/consistent-function-scoping': 'off',
+			'unicorn/no-array-for-each': 'off',
+			'unicorn/prefer-spread': 'off',
+			'unicorn/prefer-module': 'off',
+		},
+	},
+	{
+		files: ['**/*.vue'],
+		rules: {
+			'vue/valid-v-slot': 'warn',
+		},
+	},
 ];
